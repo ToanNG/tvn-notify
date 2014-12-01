88e6ab3d9f1d264ba8540191c492e8d9cce2f618
ToanNG <toan.nguyen@2359media.com.vn>
2014-12-02 01:23:19 +0700

    first-commit

---
+++ socket-server/app.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+  	console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
