fc42fc94d7969c304caab1574319d9530db71157
ToanNG <toan.nguyen@2359media.com.vn>
2014-12-03 01:28:10 +0700

    add demo channel for admin-ntd

--- socket-server/app.js
+++ socket-server/app.js
@@ -3,7 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  
+	socket.on('user join', function(user){
+		var rooms;
+
+		socket.user = user;
+
+		switch (user.role) {
+			case 'nhatuyendung':
+				rooms = ['ad-ntd', 'ntd-ntv'];
+				break;
+			case 'nguoitimviec':
+				rooms = ['ad-ntv', 'ntd-ntv'];
+				break;
+			case 'admin':
+				rooms = ['ad-ntd', 'ad-ntv'];
+				break;
+		}
+
+		rooms.forEach(function(room){
+			socket.join(room);
+		});
+	});
+
+	socket.on('notify', function(message, receiverId, room){
+		console.log(message, receiverId, room);
+		socket.broadcast.to(room).emit('notify', message, receiverId);
+	});
+
   socket.on('disconnect', function(){
   	console.log('user disconnected');
   });
