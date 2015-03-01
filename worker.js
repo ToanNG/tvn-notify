e384b46458a16f72419a280020acf20b6098f57f
Toan Nguyen <toan.nguyen@2359media.com.vn>
2015-03-01 12:25:04 +0700

    add documents

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var app = require('express')();
 var serveStatic = require('serve-static');
 var path = require('path');
+var request = require('request');
 
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
@@ -10,9 +11,40 @@ module.exports.run = function (worker) {
 
   scServer.addMiddleware(scServer.MIDDLEWARE_SUBSCRIBE,
     function (socket, channel, next) {
-      console.log(socket.sessionIdentification);
-      console.log(channel);
-      next();
+      var sessionId = socket.sessionIdentification;
+      var userId = channel.replace('notify-', '');
+      var isAuthenticated = false;
+
+      console.log('==========');
+      console.log(sessionId);
+      console.log(userId);
+      console.log('==========');
+
+      request.post({
+        url: 'AUTHENTICATE_API', // POST API nhận 2 param user_id and session_id, trả về json có field isAuthenticate (true hoặc false)
+        json: true,
+        body: {
+          user_id: userId,
+          session_id: sessionId
+        },
+        timeout: 5000}, function (error, response, body) {
+          if (error) {
+            next(error);
+          } else {
+            if (!error && response.statusCode == 200) {
+
+              // Sau khi có response, kiểm tra isAuthenticated. Nếu true thì allow subscribe
+              var isAuthenticated = true; // Hardcode để test
+              // var isAuthenticated = body.isAuthenticated;
+              
+              if (isAuthenticated) {
+                next(); // Allow subscribe
+              } else {
+                next('Handshake failed'); // Block
+              }
+            }
+          }
+      });
     }
   );
 
@@ -20,10 +52,6 @@ module.exports.run = function (worker) {
     socket.on('user join', function(user){
       socket.sessionIdentification = user.ssid;
       socket.emit('user join successfully');
-    });
-
-    socket.on('notify', function(data){
-      scServer.global.publish(data.receiverId, {message: data.message});
     });
 
     socket.on('benchmark', function(payload){
