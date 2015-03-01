e53a07cced5c2bd4841cb5336998999c7ccf5415
Toan Nguyen <toan.nguyen@2359media.com.vn>
2015-03-01 11:34:37 +0700

    pubsub with authentication

--- worker.js
+++ worker.js
@@ -6,26 +6,24 @@ var path = require('path');
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
   
-  // Get a reference to our raw Node HTTP server
-  var httpServer = worker.getHTTPServer();
-  // Get a reference to our realtime SocketCluster server
   var scServer = worker.getSCServer();
-  /*if (scServer.global.clients == undefined)
-    scServer.global.clients = [];
-  app.use(serveStatic(path.resolve(__dirname, 'demo')));
 
-  httpServer.on('req', app);
-  */
-  /*
-    In here we handle our incoming realtime connections and listen for events.
-    From here onwards is just like Socket.io but with some additional features.
-  */
+  scServer.addMiddleware(scServer.MIDDLEWARE_SUBSCRIBE,
+    function (socket, channel, next) {
+      console.log(socket.sessionIdentification);
+      console.log(channel);
+      next();
+    }
+  );
+
   scServer.on('connection', function (socket) {
-    /*
-      Store that socket's session for later use.
-      We will emit events on it later - Those events will 
-      affect all sockets which belong to that session.
-    */
+    socket.on('user join', function(user){
+      socket.sessionIdentification = user.ssid;
+    });
+
+    socket.on('notify', function(data){
+      scServer.global.publish(data.receiverId, {message: data.message});
+    });
 
     socket.on('benchmark', function(payload){
       console.log(payload);
@@ -34,6 +32,5 @@ module.exports.run = function (worker) {
   
   scServer.on('sessionEnd', function (ssid) {
     console.log("Session " + ssid + " end");
-    //clients[socket.user.id] = undefined;
   });
 };
